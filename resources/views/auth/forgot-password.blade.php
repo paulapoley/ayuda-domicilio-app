23de7ad6bffe35c969d8f9f82ebb5f6545d3a133
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-09-08 13:11:23 +0200

    Add files via upload

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,36 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('¿Olvidaste tu contraseña? No hay problema. Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace de restablecimiento de contraseña que le permitirá elegir una nueva.') }}
+        </div>
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button>
+                    {{ __('Enlace de restablecimiento de contraseña de correo electrónico') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
---
+++ resources/views/auth/register-profesional.blade.php
@@ -0,0 +1,114 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+                <input type="hidden" value="1" name="tipo_usuario_id">
+            @csrf
+            <div>
+                <x-label for="name" :value="__('Nombre')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+            
+            <!-- Apellidos -->
+            <div>
+                <x-label for="apellidos" :value="__('Apellidos:')" />
+
+                <x-input id="apellidos" class="block mt-1 w-full" type="text" name="apellidos" :value="old('apellidos')" required autofocus />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Confirm Email -->
+            <div class="mt-4">
+                <x-label for="email_confirmation" :value="__('Confirm Email')" />
+
+                <x-input id="email_confirmation" class="block mt-1 w-full"
+                                type="email"
+                                name="email_confirmation" required />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Contraseña')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                         type="password"
+                         name="password"
+                         required autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                         type="password"
+                         name="password_confirmation" required />
+            </div>
+
+            <!-- Fecha Nacimiento -->
+            <div class="mt-4">
+                <x-label for="fechaNacimiento" :value="__('Fecha Nacimiento')" />
+
+                <x-input id="fechaNacimiento" class="block mt-1 w-full"
+                         type="date"
+                         name="fechaNacimiento"
+                         :value="old('fechaNacimiento')"
+                         required />
+            </div>
+             <!-- Genero -->
+            <div class="mt-4">
+                <x-label for="genero" :value="__('Género:')" />
+
+                <x-select id="genero" name="genero" required>
+                    <option value="">{{__('Elige una opción')}}</option>
+                    <option value="2" @if (old('genero') == 2) selected @endif>{{__('No binario')}}</option>
+                    <option value="1" @if (old('genero') == 1) selected @endif>{{__('Masculino')}}</option>
+                    <option value="0" @if (old('genero') == 0) selected @endif>{{__('Femenino')}}</option>
+                </x-select>
+            </div>
+
+             <!-- dni -->
+             <div>
+                <x-label for="dni" :value="__('Dni:')" />
+
+                <x-input id="dni" class="block mt-1 w-full" type="text" name="dni" :value="old('dni')" required autofocus />
+            </div>
+
+             <!-- Tipo profesional -->
+             <div class="mt-4">
+                <x-label for="tipoProfesional" :value="__('Tipo de profesional:')" />
+
+                <x-select id="tipoProfesional" name="tipoProfesional" required>
+                    <option value="">{{__('Elige una opción')}}</option>
+                    <option value="1" @if (old('genero') == 1) selected @endif>{{__('Enfermero/a')}}</option>
+                    <option value="0" @if (old('genero') == 0) selected @endif>{{__('Auxiliar')}}</option>
+                </x-select>
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('¿Ya registrado?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Registrar') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
